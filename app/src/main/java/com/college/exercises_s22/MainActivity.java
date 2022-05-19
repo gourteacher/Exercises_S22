9cd62ffb6f0125a0eb178768da23de80252c3aa7
gourteacher <ouaazs@algonquincollege.com>
2022-05-18 23:05:10 -0400

    Week2 - Relative Layout

--- app/src/main/java/com/college/exercises_s22/MainActivity.java
+++ app/src/main/java/com/college/exercises_s22/MainActivity.java
@@ -2,13 +2,6 @@ package com.college.exercises_s22;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.TextView;
-
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid); //shows the layout
-
+        setContentView(R.layout.activity_main_relative); //shows the layout
     }
 }
