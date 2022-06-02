7ca64e97ba7bbc17c2cfbe8fc68d882c5033fb4e
gourteacher <ouaazs@algonquincollege.com>
2022-06-02 02:33:54 -0400

    Activities and intents : contains intents, shared preferences, and ActivityResult

---
+++ app/src/main/java/com/college/exercises_s22/ActivityIntentExample.java
@@ -0,0 +1,68 @@
+package com.college.exercises_s22;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ActivityIntentExample extends AppCompatActivity {
+
+    ActivityResultLauncher<String> mPhotoLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_example);
+
+        //This shows how to use Android's default email app to send an email:
+        Button temp = findViewById(R.id.sendEmailExample);
+        temp.setOnClickListener(click ->{
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"email@example.com"});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "subject here");
+            intent.putExtra(Intent.EXTRA_TEXT, "body text");
+            startActivity(intent);
+
+        });
+
+        //This shows how to use Android's default web view app to view a web page:
+        temp = findViewById(R.id.viewURL);
+        temp.setOnClickListener( click -> {
+
+            String url = "http://www.algonquincollege.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData( Uri.parse(url) );
+            startActivity(i);
+
+        });
+
+        //This shows how to use Android's default web view app to view a web page:
+        temp = findViewById(R.id.makePhoneCall);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "tel:" + "6137274700";
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse(url));
+
+                int permissionCheck = ContextCompat.checkSelfPermission(ActivityIntentExample.this, Manifest.permission.CALL_PHONE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                    if (i.resolveActivity(getPackageManager()) != null)
+                        startActivity(i);
+                }
+            }
+        } );
+
+        Button term = findViewById( R.id.intent_return_button);
+        term.setOnClickListener(  click ->  { finish(); } );
+    }
+}
--- app/src/main/java/com/college/exercises_s22/FirstActivity.java
+++ app/src/main/java/com/college/exercises_s22/FirstActivity.java
@@ -1,42 +1,98 @@
 package com.college.exercises_s22;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
 
-    @Override
+    public final static String TAG ="FirstActivity";
+
+    public final static String PREFERENCES_FILE = "MyData";
+
+    @Override     //first called
     protected void onCreate(Bundle savedInstanceState) {
-        //Your program starts here
-        super.onCreate(savedInstanceState);
-        // setContentView loads objects onto the screen.
-        // Before this function, the screen is empty.
-        setContentView(R.layout.activity_first);
-        Button secondButton = findViewById(R.id.buttonToSecond);
-        EditText inputText = findViewById(R.id.inputText);
+        super.onCreate(savedInstanceState); //calls parent onCreate()
 
-        //This creates a transition to load SecontActivity.java:
-        Intent nextPage = new Intent(this, SecondActivity.class);
-        //when you click the button, start the next activity:
-        secondButton.setOnClickListener( click ->
+        Log.i(TAG, "In onCreate, creating the objects");
+        setContentView( R.layout.activity_first ); //loads XML on screen
+
+        SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        //Read preferences
+        String previous = prefs.getString("ReserveName", this.getResources().getString(R.string.default_value_str));
+        TextView edit = findViewById(R.id.reserved_name);
+        edit.setText(previous);
+
+        Button btn = findViewById(R.id.start_button);
+        btn.setOnClickListener(  (  click ) ->
         {
-            nextPage.putExtra("name", "me");      //name = Eric
-            nextPage.putExtra("age", 20);           // age = 20
-            nextPage.putExtra("typed", inputText.getText().toString() ); //typed = what user typed
-            startActivity(nextPage);    //go to SecondActivity.java
+            //Where you are     //where we're going
+            Intent nextPage = new Intent(FirstActivity.this,   SecondActivity.class  );
+            //Make the transition:
+            startActivity(nextPage);
+
+        } ); //OnCLickListener goes in here
+
+
+        Button btn2 = findViewById(R.id.intent_examples);
+        btn2.setOnClickListener( (  click ) ->
+        {
+            Intent nextPage = new Intent(FirstActivity.this,   ActivityIntentExample.class  );
+            //Make the transition:
+            startActivity(    nextPage  );
+        }); //OnCLickListener goes in here
+
+        //SharedPreferencesExample
+        Button btn3 = findViewById(R.id.shared_preferences);
+        btn3.setOnClickListener( (  click ) ->
+        {
+            EditText userText = findViewById(R.id.user_input);
+            String userTyped = userText.getText().toString();
+            Intent nextPage = new Intent(FirstActivity.this,   SharedPreferencesExample.class  );
+
+            nextPage.putExtra("USERINPUT", userTyped);
+            nextPage.putExtra("MONTH", 10);
+            nextPage.putExtra("OTHER INFO", 3.14);
+
+            //Make the transition:
+            startActivity(    nextPage  );
         });
-
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
+    @Override //screen is visible but not responding
     protected void onStart() {
         super.onStart();
+
+        Log.d(TAG, "In onStart, visible but not responding");
+    }
+
+    @Override //screen is visible but not responding
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "In onResume");
+    }
+
+    @Override //screen is visible but not responding
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "In onPause");
+    }
+
+    @Override //not visible
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "In onStop");
+    }
+
+    @Override  //garbage collected
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "In onDestroy");
     }
 }
