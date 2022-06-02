c00cf0d1dde409dc27d52d785af9c2e48483ef9b
gourteacher <ouaazs@algonquincollege.com>
2022-06-02 00:28:44 -0400

    Week4 -  State transitions using the currency converter project from week 3

--- app/src/main/java/com/college/exercises_s22/MainActivity.java
+++ app/src/main/java/com/college/exercises_s22/MainActivity.java
@@ -2,9 +2,11 @@ package com.college.exercises_s22;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final float RATE_CAD_EURO = 0.70F;
 
+    private static final String TAG = "MainActivity";
 
+    private static final String KEY_RESULT = "saved_result";
+
+    String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate called");
 
         setContentView(R.layout.activity_main);
     }
@@ -38,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
             float amount_euro = amount_cad * RATE_CAD_EURO;
 
-            String result = "" + String.format("%.2f", amount_euro) +  " euros";
+            result = "" + String.format("%.2f", amount_euro) +  " euros";
 
             tv.setText(result);
 
@@ -49,5 +57,61 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(KEY_RESULT, result); // outState { "saved_result"="12.5" }
+
+        Log.i(TAG, "onSaveInstanceState called");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle outState) {
+        super.onRestoreInstanceState(outState);
+
+        result = outState.getString(KEY_RESULT);
+
+        TextView tv = findViewById(R.id.resultId);
+
+        tv.setText(result);
+        Log.i(TAG, "onRestoreInstanceState called ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(TAG, "onPause called");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume called");
+    }
 
 }
