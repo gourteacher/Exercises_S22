6ec574c6a6d6e210deed8e1531a19b4003db6264
gourteacher <ouaazs@algonquincollege.com>
2022-06-06 17:48:15 -0400

    Week5 - Listview example

--- app/src/main/java/com/college/exercises_s22/FirstActivity.java
+++ app/src/main/java/com/college/exercises_s22/FirstActivity.java
@@ -1,42 +1,83 @@
 package com.college.exercises_s22;
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirstActivity";
+
+    private ArrayList<String> elements = new ArrayList<>(  );
+    MyListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Your program starts here
         super.onCreate(savedInstanceState);
+
         // setContentView loads objects onto the screen.
         // Before this function, the screen is empty.
         setContentView(R.layout.activity_first);
-        Button secondButton = findViewById(R.id.buttonToSecond);
-        EditText inputText = findViewById(R.id.inputText);
 
-        //This creates a transition to load SecontActivity.java:
-        Intent nextPage = new Intent(this, SecondActivity.class);
-        //when you click the button, start the next activity:
-        secondButton.setOnClickListener( click ->
-        {
-            nextPage.putExtra("name", "me");      //name = Eric
-            nextPage.putExtra("age", 20);           // age = 20
-            nextPage.putExtra("typed", inputText.getText().toString() ); //typed = what user typed
-            startActivity(nextPage);    //go to SecondActivity.java
+        ListView myList = findViewById(R.id.theListView);
+        //Line 37 is the same as lines 35 and 36:
+        //ListAdapter myAdapter = new MyListAdapter();
+        //myList.setAdapter( myAdapter);
+        myList.setAdapter( myAdapter = new MyListAdapter());
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener( click -> {
+
+            Log.i(TAG, "Adding a row");
+            elements.add( "Another Row");
+            myAdapter.notifyDataSetChanged();
+        });
+
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                elements.remove(i);
+                myAdapter.notifyDataSetChanged();
+            }
         });
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+    private class MyListAdapter extends BaseAdapter {
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        public int getCount() { return elements.size();}
+
+        public Object getItem(int position) { return "This is row " + position; }
+
+        public long getItemId(int position) { return position; }
+
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            LayoutInflater inflater = getLayoutInflater();
+
+            //make a new row:
+            View newView = inflater.inflate(R.layout.row_layout, parent, false);
+
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText( getItem(position).toString() );
+
+            //Button b =  newView.findViewById(R.id.textGoesHere);
+            //b.setText( getItem(position).toString() );
+
+            //return it to be put in the table
+            return newView;
+        }
     }
 }
