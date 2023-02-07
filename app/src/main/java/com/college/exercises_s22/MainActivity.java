426d709f2ce526a50f0a109e9af2633ed28b49ff
gourteacher <ouaazs@algonquincollege.com>
2023-02-07 15:56:11 -0500

    ViewBinding example - update build.gradle

---
+++ app/src/main/java/com/college/exercises_s22/MainActivity.java
@@ -0,0 +1,30 @@
+package com.college.exercises_s22;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.college.exercises_s22.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setContentView(binding.getRoot());
+
+        binding.computeButton.setOnClickListener(c -> {
+            String userInput = binding.userInputId.getText().toString();
+
+            binding.resultTv.setText(userInput);
+        });
+
+    }
+}
