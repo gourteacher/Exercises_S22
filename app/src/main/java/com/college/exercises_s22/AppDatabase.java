1ff7aa0a05dfaec71de84d49c240c7048daf19cb
gourteacher <ouaazs@algonquincollege.com>
2023-03-04 14:09:08 -0500

    Room example using view binding

--- app/src/main/java/com/college/exercises_s22/AppDatabase.java
+++ app/src/main/java/com/college/exercises_s22/AppDatabase.java
@@ -6,6 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+
 @Database(entities = {Person.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase minstance;
--- app/src/main/java/com/college/exercises_s22/MainActivity.java
+++ app/src/main/java/com/college/exercises_s22/MainActivity.java
@@ -2,36 +2,64 @@ package com.college.exercises_s22;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.college.exercises_s22.databinding.ActivityMainBinding;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     private AppDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         db = AppDatabase.getInstance(this);
 
-        Button btnSave = findViewById(R.id.btn_save);
-        btnSave.setOnClickListener(v -> {
+        binding.btnSave.setOnClickListener(v -> {
             saveData();
+        });
+
+        binding.btnClear.setOnClickListener( v -> {
+            clearData();
+        });
+
+        binding.btnList.setOnClickListener( v -> {
+            listData();
         });
     }
 
 
     private void saveData() {
-        final String mlastname = ((TextView) findViewById(R.id.lname_edit)).getText().toString();
-        final String mfirstname = ((TextView) findViewById(R.id.fname_edit)).getText().toString();
+        String lastname = binding.lnameEdit.getText().toString();
+        String firstname = binding.fnameEdit.getText().toString();
         new Thread(() -> {
-            Person person = new Person(mlastname, mfirstname);
+            Person person = new Person(lastname, firstname);
             PersonDAO dao = db.getPersonDAO();
             dao.insertPerson(person);
+            List<Person> people = dao.listPeople();
+            for(Person p:people) {
+                System.out.printf("%s, %s\n", p.last_name, p.first_name );
+            }
+        }).start();
+    }
+
+    private void  clearData() {
+        binding.lnameEdit.setText("");
+        binding.fnameEdit.setText("");
+    }
+
+    private void listData() {
+        new Thread(() -> {
+            PersonDAO dao = db.getPersonDAO();
             List<Person> people = dao.listPeople();
             for(Person p:people) {
                 System.out.printf("%s, %s\n", p.last_name, p.first_name );
