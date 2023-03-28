191cd5f95155e66878c044837ac8164bea7f38d9
gourteacher <ouaazs@algonquincollege.com>
2023-03-28 19:27:04 -0400

    Question 2

--- app/src/main/java/com/college/exercises_s22/HealthActivity.java
+++ app/src/main/java/com/college/exercises_s22/HealthActivity.java
@@ -1,14 +1,31 @@
 package com.college.exercises_s22;
 
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+
+//This code has multiple run-time errors
+// Fix them
+// Then make it display properly the ListView
 public class HealthActivity extends AppCompatActivity {
+
+    ArrayList<String> people;
+    ListView listview = findViewById(R.id.listView);
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+
+        //Populate the ArrayList
+        people.addAll(0, Arrays.asList(MyData.clientList));
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, people);
+
     }
 }
---
+++ app/src/main/java/com/college/exercises_s22/MyData.java
@@ -0,0 +1,12 @@
+package com.college.exercises_s22;
+
+
+public class MyData {
+
+    public static String[] clientList = {
+            "John Monroe",
+            "Bill Cassidy",
+            "Jean Ferrat"
+    };
+
+}
