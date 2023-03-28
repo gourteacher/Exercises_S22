8a36d9615dd762b28504c05927a3d29eb7f7181d
gourteacher <ouaazs@algonquincollege.com>
2023-03-28 19:01:05 -0400

    Question 1

---
+++ app/src/main/java/com/college/exercises_s22/HealthActivity.java
@@ -0,0 +1,14 @@
+package com.college.exercises_s22;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+    }
+}
